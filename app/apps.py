3f47d13a04767fa7f499f281cb6c6c19333add7f
unknown <alhaqyrobbi@gmail.com>
2025-04-18 15:39:12 +0700

    Fitur Uploads + setting penamaan database

--- app/apps.py
+++ app/apps.py
@@ -1,6 +1,15 @@
 from django.apps import AppConfig
+from django.db import connection
+from django.db.utils import OperationalError
 
 
 class AppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'app'
+
+    def ready(self):
+        try:
+            connection.ensure_connection()
+            print("✅ Berhasil terhubung ke database:", connection.settings_dict['NAME'])
+        except OperationalError:
+            print("❌ Gagal terhubung ke database")
