36327a71753e8d3f0ce7dadcdf24ea74ea88071b
unknown <raihanrizky1308@gmail.com>
2025-04-19 10:39:43 +0700

    menambahkan algoritma admin,user,staff

--- app/models.py
+++ app/models.py
@@ -1,6 +1,16 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+class Profile(models.Model):
+    ROLE_CHOICES = [
+        ('admin', 'Admin'),
+        ('staff', 'Staff'), 
+        ('user', 'User'),
+    ]
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='user')
+    
 class Create(models.Model):
     gambar = models.ImageField(upload_to='uploads/')
     kategori = models.CharField(max_length=100)
