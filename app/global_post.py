0a8b543b7ae48fcf2af324fdc725152922ffb11b
M Zidan Kusuma Firdaus <zidankusumafirdaus.school@gmail.com>
2025-04-26 23:15:57 +0700

    add search & update html

--- app/global_post.py
+++ app/global_post.py
@@ -4,6 +4,22 @@ from .models import Create
 
 @login_required
 def global_gallery(request):
-    gambar_list = Create.objects.filter(status='a').order_by('-tanggal_upload')
-    return render(request, 'global_post.html', {'gambar_list': gambar_list})
+    query = request.GET.get('search', '')  # Ambil parameter pencarian dari URL
+    kategori_filter = request.GET.get('kategori', None)  # Ambil kategori dari URL
 
+    # Ambil semua kategori yang unik
+    kategori_list = Create.objects.values_list('kategori', flat=True).distinct()
+
+    if kategori_filter:
+        gambar_list = Create.objects.filter(status='a', kategori=kategori_filter).order_by('-tanggal_upload')
+    elif query:
+        gambar_list = Create.objects.filter(status='a', judul__icontains=query).order_by('-tanggal_upload')
+    else:
+        gambar_list = Create.objects.filter(status='a').order_by('-tanggal_upload')
+    
+    return render(request, 'global_post.html', {
+        'gambar_list': gambar_list,
+        'query': query,
+        'kategori_list': kategori_list,
+        'kategori_filter': kategori_filter,
+    })
