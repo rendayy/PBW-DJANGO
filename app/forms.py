cbc217e6832a95d85f29c2de3a884117046c9415
unknown <alhaqyrobbi@gmail.com>
2025-04-27 16:37:18 +0700

    revisi login register

--- app/forms.py
+++ app/forms.py
@@ -7,10 +7,27 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 class RegisterForm(UserCreationForm):
+    email = forms.EmailField(
+        required=True,
+        widget=forms.EmailInput(attrs={'placeholder': 'Enter your email'})
+    )
+
+    username = forms.CharField(
+        widget=forms.TextInput(attrs={'placeholder': 'Enter your username'})
+    )
+
+    password1 = forms.CharField(
+        widget=forms.PasswordInput(attrs={'placeholder': 'Enter your password'})
+    )
+
+    password2 = forms.CharField(
+        widget=forms.PasswordInput(attrs={'placeholder': 'Confirm your password'})
+    )
+
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
-
+        
 class CreateForm(forms.ModelForm):
     class Meta:
         model = Create
